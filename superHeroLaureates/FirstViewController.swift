e20c7c4e284e2fe47342f96aebdac9aaf8df9aa7
Paudel,Saroj <s527993@ol10016m.nwmissouri.edu>
2019-04-13 13:47:02 -0500

    Initial Commit

---
+++ superHeroLaureates/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  superHeroLaureates
+//
+//  Created by Paudel,Saroj on 4/13/19.
+//  Copyright © 2019 Paudel,Saroj. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ superHeroLaureates/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  superHeroLaureates
+//
+//  Created by Paudel,Saroj on 4/13/19.
+//  Copyright © 2019 Paudel,Saroj. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
